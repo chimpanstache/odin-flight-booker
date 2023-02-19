b9fe58414a5c478e86287f94155ad3485a08ba36
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2023-02-19 17:48:26 +0100

    seeding airports and flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Airport.create(country: Faker::Address.country,
+                 city: Faker::Address.city,
+                 name: Faker::Name.unique.name,
+                 code: [*'A'..'Z'].sample(3).join)
+end
+
+10.times do
+  flight = Flight.new(time: 1.month.from_now,
+                      max_passenger: rand(60..500),
+                      passenger_count: 0,
+                      duration_min: rand(45..900))
+
+  flight.departure = Airport.all.sample
+  flight.arrival = Airport.where.not(id: flight.departure.id).all.sample
+end
