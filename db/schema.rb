f980d3a836a25dbc6ab98530485f5473a171b54a
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2023-02-26 13:37:20 +0100

    add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_26_090110) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_123520) do
   create_table "airports", force: :cascade do |t|
     t.string "country"
     t.string "city"
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_26_090110) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
