e93a30fefb8d4f26d3456f3d0e5ea4195834c332
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2023-04-02 19:55:45 +0200

    populate countries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_173819) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_174421) do
   create_table "airports", force: :cascade do |t|
     t.string "country"
     t.string "city"
