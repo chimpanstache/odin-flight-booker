c3e70bae50b5e2712fe26934c07c04f8059b986a
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2023-04-02 23:10:32 +0200

    create relationship between airports and countries

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,5 @@
 class Airport < ApplicationRecord
+  belongs_to :countries, class_name: 'Country', foreign_key: 'country_id'
   has_many :arrivals, foreign_key: 'arrival_id', class_name: 'Flight'
   has_many :departures, foreign_key: 'departure_id', class_name: 'Flight'
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,3 @@
 class Country < ApplicationRecord
+  has_many :airports, class_name: 'Airport', foreign_key: 'country_id', dependent: :destroy
 end
