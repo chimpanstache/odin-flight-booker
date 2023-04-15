48b8b6f3c7aa69b58084e3820a168d823d3dbecc
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2023-04-15 21:48:35 +0200

    transfer Country data creation from migration to seed file

--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Airport, type: :model do
-  it 'outputs hello world' do
-    puts 'Hello world!'
+  context 'testing Airport model validation' do  
+    before do
+      byebug
+      country = Country.find_by(name: 'France')
+      airport = Airport.create(country_id: country.id,
+                                  city: Faker::Address.city,
+                                  name: Faker::Name.unique.name,
+                                  code: [*'A'..'Z'].sample(3).join)
+    end
+
+    it 'belongs to a Country' do
+      expect(Airport.country).to be(country)
+    end
   end
 end
