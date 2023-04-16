b9da9247b4f0a1a499bb32dbb053a711c8bd18a4
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2023-04-16 19:17:35 +0200

    fix failing ci

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-Rails.application.load_seed
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -63,6 +62,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.before(:suite) do
+    Rails.application.load_seed # loading seeds
+  end
 
   config.include FactoryBot::Syntax::Methods
 end
